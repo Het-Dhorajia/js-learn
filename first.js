52a9e7e698e03642ceee9ee13524cb374038cb3f
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-10 14:24:43 +0530

    for question

--- first.js
+++ first.js
@@ -1,8 +1,7 @@
-sum = 0;
 
-for(let i=1 ; i <= 50 ; i++){
-   if ( i % 3 === 0 ){
-    console.log(i);
-   }
+
+for(let i=0 ; i <= 100 ; i++){
+    if ( i % 2 === 0){
+        console.log( i )
+    }
 }
-
