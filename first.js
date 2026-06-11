043db1cf344352c72181a89ebb9d73d5b59fdc7b
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-11 18:06:22 +0530

    for question

--- first.js
+++ first.js
@@ -1,5 +1,11 @@
-let heading = document.createElement("h1");
+let para1 = document.createElement("p");
+let para2 = document.createElement("p");
+let para3 = document.createElement("p");
 
-heading.innerText = "het"; 
+para1.innerText = "Paragraph 1";
+para2.innerText = "Paragraph 2";
+para3.innerText = "Paragraph 3";
 
-document.body.append(heading);
+document.body.append(para1);
+document.body.append(para2);
+document.body.append(para3);
