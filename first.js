fa4d0cd9aadaf3b51a5595272304b2c33c0b398a
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-11 11:37:00 +0530

    for question

--- first.js
+++ first.js
@@ -1,3 +1,17 @@
-compnies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
-compnies.push("Amazon"); 
-console.log(compnies);
+function countVowels(str) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i].toLowerCase();
+
+        if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+let vowels = countVowels("I am Het Dhorajia");
+console.log(vowels);
