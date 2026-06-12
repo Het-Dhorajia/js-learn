0881c635331e3ecd1c66453b813e908a39a443cd
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-12 16:36:44 +0530

    for question

--- first.js
+++ first.js
@@ -1,27 +1,5 @@
-let divs = document.querySelector("div");
+let btn = document.querySelector("#btn");
 
-let heading = document.createElement("h1");
-
-heading.innerHTML = "<h1>this is heading before div</h1>";
-
-document.body.prepend(heading);
-
-let para = document.createElement("p");
-
-para.innerHTML = "<p>this is para</p>";
-
-divs.prepend(para)
-
-let btn = document.createElement("button");
-
-btn.innerHTML = "<button>click here!</button>";
-
-divs.append(btn);
-
-let line = document.createElement("hr");
-
-btn.innerHTML = "<hr></hr>";
-
-document.body.append(line); 
-
-btn.remove();
+btn.addEventListener("click", function () {
+    document.body.style.display = "none";
+});
