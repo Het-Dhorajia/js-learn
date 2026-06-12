e06c7281f5d02ef92ae5378d07272673b35e0e5f
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-12 12:25:28 +0530

    for question

--- first.js
+++ first.js
@@ -1,8 +1,3 @@
-let div1 = document.querySelector("div");
+let para = document.querySelector("p");
 
-div1.innerHTML = "<h1>hii this is heading</h1>";
-
-div1.innerHTML += "<p>hii this is paragraph</p>";
-
-
-
+para.innerHTML = "<b>javascript</b>";
