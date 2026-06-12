f5d6c45719fc36139ea26cda4b654be817ed71e1
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-12 15:15:38 +0530

    for question

--- first.js
+++ first.js
@@ -1,7 +1,13 @@
-let divs = document.createElement("div");
+let list1 = document.createElement("ul");
 
-divs.innerText = "this is a notification";
+list1.innerHTML += "<li>this is list1</li>";
+let list1_show = list1.innerHTML += "<li>this is 2nd line</li>";
 
-document.body.prepend(divs);
+document.body.prepend(list1);
 
-document.body.remove(divs);
+let list2 = document.createElement("ul");
+
+list2.innerHTML += "<li>this is list2</li>";
+let list2_show = list2.innerHTML += "<li>this is 2nd line</li>";
+
+document.body.append(list2);
