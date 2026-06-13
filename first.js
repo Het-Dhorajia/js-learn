5a58ace84a57fa8234d57f93d0b97dc8c31e9b0b
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-13 13:12:18 +0530

    for OOPs

--- first.js
+++ first.js
@@ -1,9 +1,23 @@
-let div = document.querySelector("div");
-
-document.addEventListener("keydown", function (event) {
-    console.log(event.key); // check key is detected
-
-    if (event.key === "ArrowUp") {
-        div.style.backgroundColor = "blue";
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
     }
-});
+
+    viewData() {
+        console.log("Viewing website data...");
+    }
+}
+
+let user1 = new User("Het", "het@gmail.com");
+let user2 = new User("Rahul", "rahul@gmail.com");
+
+console.log(user1.name);
+console.log(user1.email);
+
+
+console.log(user2.name);
+console.log(user2.email);
+
+user1.viewData();
+user2.viewData();
